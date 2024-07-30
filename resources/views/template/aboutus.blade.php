9f46eac2aa58da7adc942265f5b8f14439a08d38
sulaksana234 <sulaksana60@gmail.com>
2024-07-30 12:01:05 +0800

    update

--- resources/views/template/aboutus.blade.php
+++ resources/views/template/aboutus.blade.php
@@ -94,22 +94,22 @@
                 </div>
             </div>
             <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
-                <h6 class="section-title bg-white text-start text-primary pe-3">About Us</h6>
+                <h6 class="section-title bg-white text-start text-gold pe-3">About Us</h6>
                 <h1 class="mb-4">Welcome to Queen International School Bali</h1>
                 <p class="mb-4">At Queen International School Bali, we are dedicated to providing exceptional education in hospitality and cruise ship management. Located in the heart of Bali, we combine the island's rich cultural heritage with world-class training and education to prepare our students for successful careers in the global hospitality industry.</p>
                 <p class="mb-4">Our curriculum is designed to offer a blend of theoretical knowledge and practical experience, ensuring that our students are well-equipped to meet the demands of the industry. We collaborate with renowned companies to offer real-world projects and internships, providing our students with invaluable hands-on experience.</p>
                 <div class="row gy-2 gx-4 mb-4">
                     <div class="col-sm-6">
-                        <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Skilled Instructors</p>
+                        <p class="mb-0"><i class="fa fa-arrow-right text-gold me-2"></i>Skilled Instructors</p>
                     </div>
                     <div class="col-sm-6">
-                        <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Classes</p>
+                        <p class="mb-0"><i class="fa fa-arrow-right text-gold me-2"></i>Classes</p>
                     </div>
                     <div class="col-sm-6">
-                        <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Collaborate</p>
+                        <p class="mb-0"><i class="fa fa-arrow-right text-gold me-2"></i>Collaborate</p>
                     </div>
                     <div class="col-sm-6">
-                        <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Curriculum</p>
+                        <p class="mb-0"><i class="fa fa-arrow-right text-gold me-2"></i>Curriculum</p>
                     </div>
                 </div>
                 {{-- <a class="btn btn-primary py-3 px-5 mt-2" href="">Read More</a> --}}
